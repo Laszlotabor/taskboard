17e2ec8cff125485f4a18edda8a091b85fac7f67
LesButcher <laszlotabor@gmail.com>
2025-05-29 13:21:28 +0200

    Initial commit

---
+++ client/src/main.ts
@@ -0,0 +1,13 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations(),
+    importProvidersFrom(DragDropModule), // 👈 Add this line
+  ],
+});
